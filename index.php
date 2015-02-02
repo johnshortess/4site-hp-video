92d3be06cc858630e0bbe100bbc9ec3f0f05c8dc
Michael Wilson <michaelw@4sitestudios.com>
2015-02-01 20:32:04 -0500

    test 3

--- index.php
+++ index.php
@@ -65,9 +65,6 @@
     </div>
   </div>
   -->
-  <!-- MIKE WAS HERE -->
-  <!-- MIKE WAS HERE --> 
-  <!-- MIKE WAS HERE --> 
     
   <div id="overlay-region-map" style="display: none;"><div id='map' class='dark'></div></div>
   <div id="overlay-superpowers-brad" class="overlay-container"></div>
